fb13ef6bec33a3a9c85504b4f565f04aed025e7f
ryan-swanson <rswanson95@gmail.com>
2022-11-30 22:21:54 -0800

    Revamped template to use flask render Added static files for stylesheet

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ model = whisper.load_model("tiny")
 ## GUI setup
 
 #     template here has two replacetags: header and paragraph
-with open('./html/page_template.html', 'r') as f:
-    html_template = f.read()
+# with open('./html/page_template.html', 'r') as f:
+#     html_template = f.read()
 
 def fill_template(header='',paragraph='',additional_html=''):
     result = html_template.replace("HEADING_REPLACETAG", header)
@@ -22,7 +22,8 @@ def fill_template(header='',paragraph='',additional_html=''):
     result = result.replace("ADDITIONALHTML_REPLACETAG", additional_html)
     with open('./html/tmp.html','w') as f:
         f.write(result)
-    return render_template('./html/tmp.html')
+    return result
+    
 
 ## file setup
 
@@ -48,28 +49,22 @@ def greet_the_guest():
     uploads = subprocess.check_output('ls ./uploads', \
                                              shell=True).decode('ascii').split('\n')[:-1]
 
-    additional_html = ''
+    additional_html = []
     for u in uploads:
-        additional_html += f'  <p>{u}'
-        additional_html += \
-              f'  <a href="http://127.0.0.1:5000/uploads/{u}" target="_blank">Listen</a>'
-        additional_html += \
-              f'  <a href="http://127.0.0.1:5000/transcribe/{u}" target="_blank">Read</a>'
-        additional_html += '</p>'
+        additional_html += {u}
 
-    additional_html += '  <p>Or try '
-    additional_html += \
-           '  <a href="http://127.0.0.1:5000/upload" target="_blank">uploading a file</a>'
-    additional_html += ' </p>'
 
-    return fill_template(header="Transcribe some audio!", \
+    return render_template('page_template.html',title="Transcribe some audio!", \
                          paragraph="Select from existing audio:", \
-                         additional_html=additional_html)
+                         additional_html=additional_html, upload=True)
+    # return fill_template(header="Transcribe some audio!", \
+    #                      paragraph="Select from existing audio:", \
+    #                      additional_html=additional_html)
 
 @app.route("/transcribe")
 def transcribe():
     transcription = model.transcribe("./uploads/audio.mp3")
-    return fill_template(header="Transcription of Audio", paragraph=transcription['text'])
+    return render_template('page_template.html',title="Transcription", paragraph=transcription['text'])
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -108,4 +103,4 @@ def download_file(name):
 @app.route('/transcribe/<name>')
 def transcribe_file(name):
     transcription = model.transcribe(os.path.join(app.config['UPLOAD_FOLDER'], name))
-    return fill_template(header="Transcription of Audio", paragraph=transcription['text'])
+    return render_template('page_template.html',title="Transcription", paragraph=transcription['text'])
